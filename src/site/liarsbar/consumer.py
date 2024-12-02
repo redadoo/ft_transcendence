c03a713283cac6d9087988592cdd8e6a2e177d2d
redadoo <edoardovocaturo@gmail.com>
2024-12-02 21:28:05 +0100

    update liarsbar

--- src/site/liarsbar/consumer.py
+++ src/site/liarsbar/consumer.py
@@ -1,10 +1,31 @@
+import asyncio
+import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
+from utilities.lobbies import Lobbies
 
 
 class  liarsBarConsumer(AsyncWebsocketConsumer):
+    lobbies = Lobbies()
 
     async def connect(self):
-        print("connetct")
+        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.room_group_name = f"liarsbar_multiplayer_{self.room_name}"
+
+        self.lobby = self.lobbies.create_lobby(self.room_name, {"players": {}})
+
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        await self.accept()
+
+        self.update_lock = asyncio.Lock()
+
+        await self.send(
+            text_data=json.dumps(
+                {
+                }
+            )
+        )
         pass
 
 
