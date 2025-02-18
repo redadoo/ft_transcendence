c298bcd11157ab2454996d8a8dffe89e7b0c0fe0
Fborroto <borrotofrancesco@gmail.com>
2025-02-18 02:33:49 +0100

    camera setup for players and init player variables

--- src/site/static/liarsbar_static/js/utils/LiarsBarPlayer.js
+++ src/site/static/liarsbar_static/js/utils/LiarsBarPlayer.js
@@ -6,11 +6,16 @@ export default class LiarsBarPlayer
 	{
 		this.socket = socket;
 		this.playerId = playerId;
-		this.hand = []
+		this.hand = []; // Mano del giocatore
+		this.status = ''; // Stato del giocatore (es. "ALIVE", "DIED")
+		this.playerTurn = false; // Indica se è il turno del giocatore
+		this.selectedCards = []; // Carte selezionate
+		this.selectedIndex = []; // Indici delle carte selezionate
+		this.doubting = false; // Indica se il giocatore sta dubitando
+		this.cardSent = false; // Indica se una carta è stata inviata
 
 		this.setUpKeys();
 	}
-
 
 	setUpKeys()
 	{
@@ -50,6 +55,44 @@ export default class LiarsBarPlayer
 					)
 				);
 			}
+		}
+	}
+
+	updateState(lobbyInfo) 
+	{
+		// Verifica che lobbyInfo.players sia un oggetto
+		if (!lobbyInfo.players || typeof lobbyInfo.players !== 'object') 
+		{
+			console.error('lobbyInfo.players non è un oggetto valido.', lobbyInfo.players);
+			return;
+		}
+	
+		// Converti l'oggetto players in un array
+		const playersArray = Object.values(lobbyInfo.players);
+	
+		// Trova il giocatore corrente nei dati ricevuti
+		const currentPlayerData = playersArray.find(
+			(player) => player.player_id === this.playerId
+		);
+	
+		if (currentPlayerData)
+		{
+			// Aggiorna la mano del giocatore
+			this.hand = currentPlayerData.hand.map(cardObj => cardObj.card); // Estrae solo il valore della carta
+	
+			// Aggiorna altre proprietà
+			this.status = currentPlayerData.status;
+			this.playerTurn = currentPlayerData.player_turn;
+			this.selectedCards = currentPlayerData.selected_cards;
+			this.selectedIndex = currentPlayerData.selected_index;
+			this.doubting = currentPlayerData.doubting;
+			this.cardSent = currentPlayerData.card_sent;
+			this.playerConnectionState = currentPlayerData.player_connection_state;
+			this.selectionIndex = currentPlayerData.selection_index;
+		}
+		else
+		{
+			console.error('Dati del giocatore non trovati nel lobbyInfo.');
 		}
 	}
 }
