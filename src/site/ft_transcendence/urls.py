3dfae25af5faa70134138de0a699054763e9e81e
redadoo <edoardovocaturo@gmail.com>
2025-03-10 01:16:51 +0100

    small tweaks

--- src/site/ft_transcendence/urls.py
+++ src/site/ft_transcendence/urls.py
@@ -13,7 +13,6 @@ urlpatterns = [
     path('', include("website.urls")),
 ]
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
